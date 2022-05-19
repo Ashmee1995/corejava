4f8ae59008ddae27c3383004764e520351310f7a
Ashmita Sharma <ashmisharma2020@gmail.com>
2022-05-19 06:30:25 -0400

    ajsdkf

---
+++ src/main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class main {
+	private static final String Boolean = null;
+
+	public static void main(String[]args) {
+		long x = 80000000000l;
+		float k = 3.6f;
+		byte c = 60;
+		 
+		System.out.println("byte = " +Byte.SIZE + " "+ Byte.MIN_VALUE+" "+ Byte.MAX_VALUE);
+		System.out.println("byte= "+Integer.SIZE +" "+Integer.MIN_VALUE+" "+Integer.MAX_VALUE);
+		System.out.println("byte= "+Long.SIZE +" "+Long.MIN_VALUE+" "+Long.MAX_VALUE);
+		System.out.println("byte= "+Float.SIZE +" "+Float.MIN_VALUE+" "+Float.MAX_VALUE);
+		System.out.println("byte= "+Character .SIZE +" "+Character.MIN_VALUE+" "+Character.MAX_VALUE);
+		System.out.println("byte= "+Short.SIZE +" "+Byte.MIN_VALUE+" "+Short.MAX_VALUE);
+		System.out.println("byte= "+Double .SIZE +" "+Byte.MIN_VALUE+" "+Double.MAX_VALUE);
+		
+		
+		
+		
+		
+		int a;
+		int b;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Value of a");
+		a = sc. nextInt();
+		
+		System.out.println ("Enter value of b");
+		b = sc.nextInt();
+		
+		int s = a + b;
+		System.out.println("Total = "+s);
+		
+		
+	}
+
+	private static String Boolean(boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+	}
+	
+
+  
+
